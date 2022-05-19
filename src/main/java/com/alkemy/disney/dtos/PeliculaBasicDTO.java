ccdb33f15fba08dfc520ea601f671e24a8f0fe1f
Patricio Castellano <patocastell@hotmail.com>
2022-05-19 00:02:15 -0300

    agrego dto Basic

--- src/main/java/com/alkemy/disney/dtos/PeliculaBasicDTO.java
+++ src/main/java/com/alkemy/disney/dtos/PeliculaBasicDTO.java
@@ -1,2 +1,17 @@
-package com.alkemy.disney.dtos;public class PeliculaBasicDTO {
+package com.alkemy.disney.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+public class PeliculaBasicDTO {
+    private Long id;
+    private String imagen;
+    private String titulo;
+    @DateTimeFormat(pattern = "yyyy/mm/dd")
+    private LocalDate fechaCreacion;
+
 }
--- src/main/java/com/alkemy/disney/dtos/PersonajeBasicDTO.java
+++ src/main/java/com/alkemy/disney/dtos/PersonajeBasicDTO.java
@@ -1,2 +1,12 @@
-package com.alkemy.disney.dtos;public class PersonajeBasicDTO {
+package com.alkemy.disney.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PersonajeBasicDTO {
+    private Long id;
+    private String imagen;
+    private String nombre;
 }
