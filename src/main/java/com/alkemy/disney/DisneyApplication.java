a6153d51b92113488b7e0124e6507f4753bb01cd
Patricio Castellano <patocastell@hotmail.com>
2022-05-19 00:07:44 -0300

    propierties

--- src/main/java/com/alkemy/disney/DisneyApplication.java
+++ src/main/java/com/alkemy/disney/DisneyApplication.java
@@ -2,8 +2,9 @@ package com.alkemy.disney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class DisneyApplication {
 
 	public static void main(String[] args) {
